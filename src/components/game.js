103416e22d4a084636e9afcfb73ee985f064951f
dan.walker <dan@danwalker.dev>
2022-03-20 22:39:52 +0000

    Extract some functions and add random start

--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid } from "./base";
+import { getNeighbours, deepCopySquares } from "../util/core";
 
 export default class GameOfLife extends React.Component {
 
@@ -10,7 +11,7 @@ export default class GameOfLife extends React.Component {
       step: 0,
       squares: squares,
       running: false,
-      startingGrid: this.deepCopySquares(squares),
+      startingGrid: deepCopySquares(squares),
       mouseDown: false,
       speed: 10
     }
@@ -39,14 +40,10 @@ export default class GameOfLife extends React.Component {
   }
 
   stepForward() {
-    const squares = this.deepCopySquares(this.state.squares);
+    const squares = deepCopySquares(this.state.squares);
     for (let i = 0; i < this.props.size; i++) {
       for (let j = 0; j < this.props.size; j++) {
-        if (this.state.squares[i][j]) {
-          squares[i][j] = this.isCellStillLive(i, j);
-        } else {
-          squares[i][j] = this.isDeadCellNowAlive(i, j);
-        }
+        squares[i][j] = this.isCellAliveOnNextStep(i, j);
       }
     }
 
@@ -74,50 +71,19 @@ export default class GameOfLife extends React.Component {
     })
   }
 
-  deepCopySquares(inputSquares) {
-    const squares = Array(this.props.size)
-    for (let i = 0; i < squares.length; i++) {
-      squares[i] = [...inputSquares[i]];
-    }
-    return squares;
-  }
-
-  isCellStillLive(row, col) {
-    // get neighbours
-    const neighbours = this.getNeighbours(row, col);
+  isCellAliveOnNextStep(row, col) {
+    const neighbours = getNeighbours(this.state.squares, row, col);
 
     const liveNeighbours = neighbours.filter(e => !!e);
 
-    return liveNeighbours.length === 2 || liveNeighbours.length === 3;
-  }
-
-  isDeadCellNowAlive(row, col) {
-    const neighbours = this.getNeighbours(row, col);
-
-    const liveNeighbours = neighbours.filter(e => !!e);
-
-    return liveNeighbours.length === 3;
-  }
-
-  getNeighbours(row, col) {
-    const up = row > 0 ? this.state.squares[row - 1][col] : null;
-    const down = row < this.props.size - 1 ? this.state.squares[row + 1][col] : null;
-    const left = col > 0 ? this.state.squares[row][col - 1] : null;
-    const right = col < this.props.size - 1 ? this.state.squares[row][col + 1] : null;
-    const upLeft = row > 0 && col > 0 ? this.state.squares[row - 1][col - 1] : null;
-    const upRight = row > 0 && col < this.props.size - 1 ? this.state.squares[row - 1][col + 1] : null;
-    const downLeft = row < this.props.size - 1 && col > 0 ? this.state.squares[row + 1][col - 1] : null;
-    const downRight = row < this.props.size - 1 && col < this.props.size - 1 ? this.state.squares[row + 1][col + 1] : null;
-
-    return [up, down, left, right, upLeft, upRight, downLeft, downRight].filter(e => e != null);
+    return liveNeighbours.length === 3 || (liveNeighbours.length == 2 && this.state.squares[row][col]);
   }
 
   onSquareClick(i, j) {
     const squares = this.state.squares.slice();
     squares[i][j] = !squares[i][j]
     this.setState({
-      squares: squares,
-      lastClicked: `${i} ${j}`
+      squares: squares
     })
   }
 
@@ -131,11 +97,10 @@ export default class GameOfLife extends React.Component {
     if (!this.state.running && this.state.step === 0) {
       // save start configuration
       this.setState({
-        startingGrid: this.deepCopySquares(this.state.squares)
+        startingGrid: deepCopySquares(this.state.squares)
       })
       this.setTimer(this.state.speed);
     }
-
 
     this.setState({
       running: !this.state.running
@@ -152,8 +117,23 @@ export default class GameOfLife extends React.Component {
 
   reset() {
     this.setState({
-      squares: this.deepCopySquares(this.state.startingGrid),
+      squares: deepCopySquares(this.state.startingGrid),
       running: false,
+      step: 0
+    })
+  }
+
+  randomise() {
+    let rows = Array(this.props.size);
+    for (let i = 0; i < rows.length; i++) {
+      rows[i] = Array(this.props.size);
+      for (let j = 0; j < rows[i].length; j++) {
+        rows[i][j] = Math.random() > 0.5;
+      }
+    }
+    
+    this.setState({
+      squares: rows,
       step: 0
     })
   }
@@ -170,6 +150,7 @@ export default class GameOfLife extends React.Component {
         <button onClick={() => this.stepForward()} disabled={this.state.running}>Step</button>
         <button onClick={() => this.reset()} disabled={this.state.running}>Reset</button>
         <button onClick={() => this.clearGrid()} disabled={this.state.running}>Clear</button>
+        <button onClick={() => this.randomise()}>Random</button>
         <br/>
         <label htmlFor="speed-slider">Speed</label>
         <input type="range" min="1" max="20" defaultValue={this.state.speed} step="1" id="speed-slider" onChange={(event) => this.setTimer(event.currentTarget.value)}/>
---
+++ src/util/core.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Array<boolean>} squares Assumes a square grid passed in so that width and height are equal
+ * @param {int} row Row of the square whose neighbours to find
+ * @param {int} col Column of the square whose neighbours to find
+ * @returns 
+ */
+function getNeighbours(squares, row, col) {
+  const up = row > 0 ? squares[row - 1][col] : null;
+  const down = row < squares.length - 1 ? squares[row + 1][col] : null;
+  const left = col > 0 ? squares[row][col - 1] : null;
+  const right = col < squares.length - 1 ? squares[row][col + 1] : null;
+  const upLeft = row > 0 && col > 0 ? squares[row - 1][col - 1] : null;
+  const upRight = row > 0 && col < squares.length - 1 ? squares[row - 1][col + 1] : null;
+  const downLeft = row < squares.length - 1 && col > 0 ? squares[row + 1][col - 1] : null;
+  const downRight = row < squares.length - 1 && col < squares.length - 1 ? squares[row + 1][col + 1] : null;
+
+  return [up, down, left, right, upLeft, upRight, downLeft, downRight].filter(e => e != null);
+}
+
+function deepCopySquares(inputSquares) {
+  const squares = Array(inputSquares.length)
+  for (let i = 0; i < squares.length; i++) {
+    squares[i] = [...inputSquares[i]];
+  }
+  return squares;
+}
+
+export {
+  getNeighbours,
+  deepCopySquares
+}
