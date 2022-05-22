a30fca9a41e01b50f4987165c1f2fbbc838780b4
dan.walker <dan@danwalker.dev>
2022-05-22 21:05:07 +0100

    Use function components
    Needed to use the callback forms of setState, as the old
    value is captured in the closure. Callback form will pass
    in the current state value

--- src/components/game.js
+++ src/components/game.js
@@ -1,7 +1,166 @@
 import React from "react";
 import { Grid } from "./grid";
 import { getNeighbours, deepCopySquares, initBackingArray } from "../util/core";
+import { useState, useEffect } from "react";
 
+export default function GameOfLife(props) {
+  const initialSquares = initBackingArray(props.size);
+
+  const [step, setStep] = useState(0);
+  const [squares, setSquares] = useState(initialSquares);
+  const [running, setRunning] = useState(false);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [speed, setSpeed] = useState(10);
+  const [color, setColor] = useState('red');
+  const [startingGrid, setStartingGrid] = useState(deepCopySquares(initialSquares));
+  const [currentInterval, setCurrentInterval] = useState(null);
+
+  useEffect(() => {
+    document.body.onmousedown = () => {
+      setMouseDown(true);
+    }
+  });
+  useEffect(() => {
+    document.body.onmouseup = () => {
+      setMouseDown(false);
+    }
+  });
+
+  const stepForward = () => {
+    setStep(currentStep => currentStep + 1);
+    setSquares(currentSquares => {
+      const newSquares = deepCopySquares(currentSquares);
+      for (let i = 0; i < props.size; i++) {
+        for (let j = 0; j < props.size; j++) {
+          newSquares[i][j] = isCellAliveOnNextStep(i, j, currentSquares);
+        }
+      }
+      return newSquares;
+    });
+
+  }
+
+  const updateTimer = (speed) => {
+    if (currentInterval) {
+      clearInterval(currentInterval)
+    }
+
+    let newInterval = setInterval(() => stepForward(), 1000 / speed);
+
+    setCurrentInterval(newInterval);
+    setSpeed(speed);
+
+  }
+
+  const isCellAliveOnNextStep = (row, col, currentSquares) => {
+    const neighbours = getNeighbours(currentSquares, row, col);
+
+    const liveNeighbours = neighbours.filter(e => !!e);
+
+    return liveNeighbours.length === 3 || (liveNeighbours.length === 2 && currentSquares[row][col]);
+  }
+
+  const handleSquareClick = (i, j) => {
+    const squaresCopy = squares.slice();
+    squaresCopy[i][j] = !squaresCopy[i][j]
+
+    setSquares(squaresCopy);
+
+  }
+
+  const handleMouseOver = (i, j) => {
+    if (mouseDown) {
+      handleSquareClick(i, j)
+    }
+  }
+
+  const startOrStop = () => {
+    if (running) {
+      stop();
+    } else {
+      start();
+    }
+  }
+
+  const start = () => {
+    if (step === 0) {
+      setStartingGrid(deepCopySquares(squares));
+    }
+
+    setRunning(true);
+    updateTimer(speed);
+
+  }
+
+  const stop = () => {
+    setRunning(false);
+    clearInterval(currentInterval);
+  }
+
+  const clearGrid = () => {
+    setSquares(initBackingArray(props.size));
+    setRunning(false);
+    setStep(0);
+  }
+
+  const reset = () => {
+    setSquares(deepCopySquares(startingGrid));
+    setRunning(false);
+    setStep(0);
+  }
+
+  const randomise = () => {
+    let rows = Array(props.size);
+    for (let i = 0; i < rows.length; i++) {
+      rows[i] = Array(props.size);
+      for (let j = 0; j < rows[i].length; j++) {
+        rows[i][j] = Math.random() > 0.85;
+      }
+    }
+
+    setSquares(rows);
+    setStep(0);
+  }
+
+  const changeColor = (color) => {
+    setColor(color);
+  }
+
+  return (
+    <div>
+      <Grid
+        handleClick={(i, j) => handleSquareClick(i, j)}
+        handleMouseOver={(i, j) => handleMouseOver(i, j)}
+        color={color}
+        size={props.size}
+        squares={squares} />
+      <div className="controls">
+        <div>
+          <button className={running ? "stop" : "start"} onClick={() => startOrStop()}>{running ? "Stop" : "Start"}</button>
+          <button onClick={() => stepForward()} disabled={running}>Step</button>
+          <button onClick={() => reset()} disabled={running}>Reset</button>
+          <button onClick={() => clearGrid()} disabled={running}>Clear</button>
+          <button onClick={() => randomise()} disabled={running}>Random</button>
+        </div>
+        <div>
+          <button onClick={() => changeColor("red")}>Red</button>
+          <button onClick={() => changeColor("blue")}>Blue</button>
+          <button onClick={() => changeColor("green")}>Green</button>
+          <button onClick={() => changeColor("purple")}>Purple</button>
+          <button onClick={() => changeColor("yellow")}>Yellow</button>
+        </div>
+      </div>
+
+      <br />
+      <label htmlFor="speed-slider">Speed</label>
+      <input type="range" min="1" max="20" defaultValue={speed} step="1" id="speed-slider" onChange={(event) => updateTimer(event.currentTarget.value)} />
+      <p>Step [{step}]</p>
+      <p>Speed: {speed}</p>
+    </div>
+  )
+
+}
+/*
 export default class GameOfLife extends React.Component {
 
   constructor(props) {
@@ -57,7 +216,7 @@ export default class GameOfLife extends React.Component {
         this.stepForward()
       }
     }, 1000 / speed);
-    
+
     this.setState({
       interval: interval,
       speed: speed
@@ -72,7 +231,7 @@ export default class GameOfLife extends React.Component {
     return liveNeighbours.length === 3 || (liveNeighbours.length === 2 && this.state.squares[row][col]);
   }
 
-  onSquareClick(i, j) {
+  handleSquareClick(i, j) {
     const squares = this.state.squares.slice();
     squares[i][j] = !squares[i][j]
     this.setState({
@@ -80,9 +239,9 @@ export default class GameOfLife extends React.Component {
     })
   }
 
-  onMouseOver(i, j) {
+  handleMouseOver(i, j) {
     if (this.state.mouseDown) {
-      this.onSquareClick(i, j)
+      this.handleSquareClick(i, j)
     }
   }
 
@@ -139,7 +298,7 @@ export default class GameOfLife extends React.Component {
         rows[i][j] = Math.random() > 0.85;
       }
     }
-    
+
     this.setState({
       squares: rows,
       step: 0
@@ -156,28 +315,31 @@ export default class GameOfLife extends React.Component {
     return (
       <div>
         <Grid
-          onClick={(i, j) => this.onSquareClick(i, j)}
-          onMouseOver={(i, j) => this.onMouseOver(i, j)}
+          handleClick={(i, j) => this.handleSquareClick(i, j)}
+          handleMouseOver={(i, j) => this.handleMouseOver(i, j)}
           color={this.state.color}
           size={this.props.size}
           squares={this.state.squares} />
-        <button onClick={() => this.startOrStop()}>{this.state.running ? "Stop" : "Start"}</button>
-        <button onClick={() => this.stepForward()} disabled={this.state.running}>Step</button>
-        <button onClick={() => this.reset()} disabled={this.state.running}>Reset</button>
-        <button onClick={() => this.clearGrid()} disabled={this.state.running}>Clear</button>
-        <button onClick={() => this.randomise()} disabled={this.state.running}>Random</button>
-        <button onClick={() => this.changeColor("red")}>Red</button>
-        <button onClick={() => this.changeColor("blue")}>Blue</button>
-        <button onClick={() => this.changeColor("green")}>Green</button>
-        <button onClick={() => this.changeColor("purple")}>Purple</button>
-        <button onClick={() => this.changeColor("yellow")}>Yellow</button>
-        
-        <br/>
+        <div className="controls">
+          <button className={this.state.running ? "stop" : "start"} onClick={() => this.startOrStop()}>{this.state.running ? "Stop" : "Start"}</button>
+          <button onClick={() => this.stepForward()} disabled={this.state.running}>Step</button>
+          <button onClick={() => this.reset()} disabled={this.state.running}>Reset</button>
+          <button onClick={() => this.clearGrid()} disabled={this.state.running}>Clear</button>
+          <button onClick={() => this.randomise()} disabled={this.state.running}>Random</button>
+          <button onClick={() => this.changeColor("red")}>Red</button>
+          <button onClick={() => this.changeColor("blue")}>Blue</button>
+          <button onClick={() => this.changeColor("green")}>Green</button>
+          <button onClick={() => this.changeColor("purple")}>Purple</button>
+          <button onClick={() => this.changeColor("yellow")}>Yellow</button>
+        </div>
+
+        <br />
         <label htmlFor="speed-slider">Speed</label>
-        <input type="range" min="1" max="20" defaultValue={this.state.speed} step="1" id="speed-slider" onChange={(event) => this.updateTimer(event.currentTarget.value)}/>
+        <input type="range" min="1" max="20" defaultValue={this.state.speed} step="1" id="speed-slider" onChange={(event) => this.updateTimer(event.currentTarget.value)} />
         <p>Step [{this.state.step}]</p>
         <p>Speed: {this.state.speed}</p>
       </div>
     )
   }
 }
+*/
--- src/components/grid.js
+++ src/components/grid.js
@@ -2,34 +2,31 @@ import React from "react";
 
 function Square(props) {
   return (
-    <s 
+    <s
       className={`${props.live ? "live" : ""}`}
-      onClick={() => props.onClick(props.row, props.col)}
-      onMouseOver={() => props.onMouseOver(props.row, props.col)} />
+      onClick={() => props.handleClick(props.row, props.col)}
+      onMouseOver={() => props.handleMouseOver(props.row, props.col)} />
   )
 }
 
-class Grid extends React.Component {
-
-  render() {
-    const size = this.props.size;
-    const rows = []
-    for (let i = 0; i < size; i++) {
-      const cols = []
-      for (let j = 0; j < size; j++) {
-        cols.push((<Square 
-          key={`${i},${j}`}
-          row={i}
-          col={j}
-          live={this.props.squares[i][j]}
-          onClick={(i, j) => this.props.onClick(i, j)}
-          onMouseOver={(i, j) => this.props.onMouseOver(i, j)}
-          />))
-      }
-      rows.push(<div key={i}>{cols}</div>)
+function Grid(props) {
+  const size = props.size;
+  const rows = []
+  for (let i = 0; i < size; i++) {
+    const cols = []
+    for (let j = 0; j < size; j++) {
+      cols.push((<Square
+        key={`${i},${j}`}
+        row={i}
+        col={j}
+        live={props.squares[i][j]}
+        handleClick={(i, j) => props.handleClick(i, j)}
+        handleMouseOver={(i, j) => props.handleMouseOver(i, j)}
+      />))
     }
-    return <div id="grid" className={`${this.props.color}`}>{rows}</div>;
+    rows.push(<div key={i}>{cols}</div>)
   }
+  return <div id="grid" className={`${props.color}`}>{rows}</div>;
 }
 
 export {
